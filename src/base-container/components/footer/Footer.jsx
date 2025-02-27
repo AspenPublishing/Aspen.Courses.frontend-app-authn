4317e74ec7de3e0409a40af3e78524a730c1ea24
Amir Tadrisi <amirtds@gmail.com>
2025-02-27 15:13:19 -0500

    chore: change footer link to light gray

--- src/base-container/components/footer/Footer.jsx
+++ src/base-container/components/footer/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
               <a href="https://support.aspenpublishing.com/hc/en-us/categories/19204583377428-JD-Next" target="_blank" rel="noopener" aria-label="Support">Support</a>
             </h3>
           </div>
-          <div className="flex flex-col gap-4 text-gray-100">
+          <div className="flex flex-col gap-4 text-light-gray">
             <a href={`${getConfig().LMS_BASE_URL}/tos`} className="footer-link-extra">Terms of Service</a>
             <a href={`${getConfig().LMS_BASE_URL}/privacy`} className="footer-link-extra">Privacy Policy</a>
             <a href={`${getConfig().LMS_BASE_URL}/disclosure`} className="footer-link-extra">California Consumer Act Policy</a>
@@ -32,12 +32,12 @@ const Footer = () => {
       </div>
       <hr className="my-8 border-t border-d6ae75 mx-6 lg:mx-24 container-xl mx-auto" aria-hidden="true" />
       <div className="mx-auto flex flex-col lg:flex-row justify-between lg:items-center px-6 lg:px-0 py-8 container-xl">
-        <span className="text-gray-100 text-sm mb-4 lg:mb-0">&copy; {currentYear} ASPEN PUBLISHING</span>
+        <span className="text-light-gray text-sm mb-4 lg:mb-0">&copy; {currentYear} ASPEN PUBLISHING</span>
         <div className="flex gap-4 lg:gap-8">
-          <a href="https://www.facebook.com/profile.php?id=61555997104704" className="text-gray-100 text-sm">Facebook</a>
-          <a href="https://www.linkedin.com/company/aspenpublishing/" className="text-gray-100 text-sm">LinkedIn</a>
-          <a href="https://twitter.com/AspenPublishing" className="text-gray-100 text-sm">Twitter</a>
-          <a href="https://www.youtube.com/@aspenpublishing6830" className="text-gray-100 text-sm">YouTube</a>
+          <a href="https://www.facebook.com/profile.php?id=61555997104704" className="text-light-gray text-sm">Facebook</a>
+          <a href="https://www.linkedin.com/company/aspenpublishing/" className="text-light-gray text-sm">LinkedIn</a>
+          <a href="https://twitter.com/AspenPublishing" className="text-light-gray text-sm">Twitter</a>
+          <a href="https://www.youtube.com/@aspenpublishing6830" className="text-light-gray text-sm">YouTube</a>
         </div>
       </div>
     </footer>
